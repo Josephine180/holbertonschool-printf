f0f16147ffddc455236203182a9d776c07fefbc3
Coco29dev <10059@holbertonstudents.com>
2024-11-25 15:58:22 +0000

    dossier structure main.h

--- main.h
+++ main.h
@@ -3,14 +3,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 typedef struct print
 {
 char *specifier;
-int (*f)
+int (*f)(va_list args);
+} print_t;
 
-
-
+int _putchar(char c);
 
 #endif
