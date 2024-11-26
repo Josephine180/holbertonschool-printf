eb3a2925e3a69faf554b5cd8b07dd184c4034ae0
Coco29dev <10059@holbertonstudents.com>
2024-11-26 16:33:03 +0000

    recuperation dossier branche principale

--- main.h
+++ main.h
@@ -5,13 +5,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _printf(const char *format, ...);
+/**
+* struct print - structure link a format and a function
+* @specifier: type of elements
+* @f: function link to show the type of argument
+*/
+
 typedef struct print
 {
 char *specifier;
 int (*f)(va_list args);
 } print_t;
 
-int _putchar(char c);
-int _printf(const char *format, ...);
+int _putchar (char c);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_pourcent(va_list args);
+
+
+
 
 #endif
--- printf_functions.c
+++ printf_functions.c
@@ -1,1 +1,49 @@
-get_op_func - Via le tableau, selection des specifiers
+#include "main.h"
+/**
+* _printf - print the elts by the format
+* @format: string of characters
+* Description: printf print a character
+* Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+print_t  print_t [] = {
+{"c", print_char},
+{"s", print_str},
+{"%", print_pourcent},
+{'\0', NULL}
+};
+va_list args;
+int i = 0, count = 0, j, found = 0;
+va_start(args, format);
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+for (j = 0; print_t[j].specifier != NULL; j++)
+{
+if (format[i] == *print_t[j].specifier)
+{
+count += print_t[j].f(args);
+found = 1;
+break;
+}
+}
+if (!found)
+{
+_putchar('%');
+count++;
+}
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+va_end(args);
+return (count);
+}
