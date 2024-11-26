297e68778281f0be59c047ff8fef0b0f171d26d8
Josephine180 <pierre_josephine@hotmail.com>
2024-11-26 10:36:30 +0100

    add array and va_list test

--- main.h
+++ main.h
@@ -1,2 +1,22 @@
-yyes
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int _printf(const char *format, ...);
+
+typedef struct print
+{
+char *specifier;
+int (*f)(va_list args);
+}prin_t;
+
+int _putchar (char c);
+
+
+
+
+
+#endif
--- printf_functions.c
+++ printf_functions.c
@@ -1,1 +1,41 @@
-get_op_func - Via le tableau, selection des specifiers
+#include "main.h"
+
+/*
+* -printf - print the elts by the format
+* @format: string of characters
+* Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+print_t  specifier [] = {
+{"c", print_char},
+{"s", print_str},
+{"%", print_pourcent},
+{NULL, NULL}
+};
+va_list args;
+int count = 0;
+int i = 0;
+va_start(args, format);
+if (format[i] == 0)
+{
+return(-1);
+}
+while (format && format[i])
+{
+while (print_t[count].specifier)
+{
+if (*print_t[count].specifier == format[i])
+{
+print_t[count].f(args);
+}
+count++
+}
+count = 0;
+i++
+}
+_putchar("\n");
+va_end(args);
+return(count);
+}
