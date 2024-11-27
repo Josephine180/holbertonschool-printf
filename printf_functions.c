cfc78f9980d50a66099661a6be119955de721644
Josephine180 <pierre_josephine@hotmail.com>
2024-11-27 13:49:10 +0100

    commit pour changer de branche et faire des tests

--- printf_functions.c
+++ printf_functions.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+* _printf - print the elts by the format
+* @format: string of characters
+* Description: printf print a character
+* Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+print_t  print_t[] = {
+{"c", print_char},
+{"s", print_str},
+{"%", print_pourcent},
+{"d", print_int},
+{"i", print_int},
+{NULL, NULL}
+};
 va_list args;
 int i = 0, count = 0, j, found = 0;
 va_start(args, format);
