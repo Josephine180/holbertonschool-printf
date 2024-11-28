a4a4e0a8e34729d783c87907fd413f053833c024
Josephine180 <pierre_josephine@hotmail.com>
2024-11-28 16:05:41 +0100

    description printf

--- printf_functions.c
+++ printf_functions.c
@@ -5,44 +5,44 @@
 * Description: printf print a character
 * Return: number of characters printed
 */
-int _printf(const char *format, ...)
+int _printf(const char *format, ...)/* printf prend une chaine de format et nombre variable d'arg. utiisation va_list */
 {
-print_t  print_t[] = {
+print_t  print_t[] = {/* tableau de structures def chaque elt avec une fonction et des specificateurs */
 {"c", print_char}, {"s", print_str}, {"%", print_pourcent},
 {"i", print_int}, {"d", print_int}, {NULL, NULL}
-};
-va_list args;
-int i = 0, count = 0, j;
-va_start(args, format);
-while (format[i])
+};/* chaque elt associe tableau et specificateur de format à une fonction qui affiche l'elt */
+va_list args;/* initialisation de args utilisé pour acceder aux args*/
+int i = 0, count = 0, j;/*initialisation des valeurs*/
+va_start(args, format); /* début */
+while (format[i])/* boucle externe */
 {
-if (format[i] == '%')
+if (format[i] == '%')/*condition de i */
 {
 i++;
-if (!format[i])
+if (!format[i]) /* si le format se termine par % on sort de la boucle */
 break;
-for (j = 0; print_t[j].specifier; j++)
+for (j = 0; print_t[j].specifier; j++) /* recherche du spécificateur dans le tableau */
 {
-if (format[i] == *print_t[j].specifier)
+if (format[i] == *print_t[j].specifier)/* si specificateur de format = fonction correspondant*/
 {
-count += print_t[j].f(args);
+count += print_t[j].f(args);/* appel de la fonction associée au specificateur, += nrb caractere ajouté à count*/
 break;
 }
 }
-if (!print_t[j].specifier)
+if (!print_t[j].specifier)/* si specificateur est ps trouvé le % est imprimé suivi du caractere suivant */
 {
 _putchar('%');
 _putchar(format[i]);
-count += 2;
+count += 2; /* 2 car 1 pour le % et 1 pour le caractere suivant */
 }
 }
 else
 {
-_putchar(format[i]);
-count++;
+_putchar(format[i]);/* si le caractere pas un % il est imprimé tel quel */
+count++; /*stockage des caracteres dans count */
 }
-i++;
+i++;/* continue le tableau */
 }
-va_end(args);
-return (count);
+va_end(args); /* libérer les args */
+return (count);/* retourne le nombre de caracteres */
 }
