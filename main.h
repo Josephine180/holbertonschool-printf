c9c8506d1c9fe4a72038fd3aff96268f239c1c63
Josephine180 <pierre_josephine@hotmail.com>
2024-11-28 16:32:33 +0100

    description main .h

--- main.h
+++ main.h
@@ -6,25 +6,25 @@
 #include <unistd.h>
 #include <limits.h>
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...);/* declaration fonction */
 /**
 * struct print - structure link a format and a function
 * @specifier: type of elements
 * @f: function link to show the type of argument
 */
 
-typedef struct print
+typedef struct print /* creation de la structure */
 {
-char *specifier;
-int (*f)(va_list args);
-} print_t;
+char *specifier;/* pointeur qui va pointer sur le tableau */
+int (*f)(va_list args);/* declaration d'un pointeur de fonction et argument */
+} print_t;/* alias du nom */
 
-int _putchar (char c);
-int print_char(va_list args);
-int print_str(va_list args);
-int print_pourcent(va_list args);
-int print_int(va_list args);
-int print_num(unsigned int num);
+int _putchar (char c);/*declaration des fonctions utilisées dans l'autre fichier */
+int print_char(va_list args);/* same */
+int print_str(va_list args);/* same */
+int print_pourcent(va_list args);/* same */
+int print_int(va_list args); /* same */
+int print_num(unsigned int num); /* same*/
 
 
 #endif
