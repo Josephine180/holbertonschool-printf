cd8fa26cdde239e84ab26700f20331e6602c87ba
Josephine180 <pierre_josephine@hotmail.com>
2024-11-28 16:16:56 +0100

    description et commentaire types_function

--- type_functions.c
+++ type_functions.c
@@ -7,12 +7,12 @@
 * Return: number of character
 */
 
-int print_char(va_list args)
+int print_char(va_list args)/* fonction qui print char */
 {
 char c;
-c = va_arg(args, int);
-_putchar(c);
-return (1);
+c = va_arg(args, int); /* eextraction arg de type char depuis va_list */
+_putchar(c);/*impression du caractere via _putchar */
+return (1);/* retourne le nombre de caractere imprimé */
 }
 
 /**
@@ -22,22 +22,22 @@ return (1);
 * Return: number of character of a string.
 */
 
-int print_str(va_list args)
+int print_str(va_list args)/* fonction print int */
 {
-char *s;
-int count = 0;
-s = va_arg(args, char *);
+char *s;/* pointeur d'une chaine de caracteres*/
+int count = 0;/*initialisation count */
+s = va_arg(args, char *);/* extraction arg de type string depuis va_list */
 if (s == NULL)
 {
-s = "(null)";
+s = "(null)"; /* si chaine null on affiche null */
 }
-while (*s)
+while (*s)/* on parcourt la chaine caractère par caractère */
 {
-_putchar(*s);
-s++;
-count++;
+_putchar(*s); /* impression de chaque caractere */
+s++;/* déplacement vers le caractere suivant */
+count++; /* incrementation du compteur de caracteres */
 }
-return (count);
+return (count);/*retourne le nombre de caracteres */
 }
 
 /**
@@ -49,7 +49,7 @@ return (count);
 
 int print_pourcent(va_list args)
 {
-(void)args;
+(void)args;/* argument args pas utilisé */
 _putchar('%');
 return (1);
 }
@@ -60,17 +60,18 @@ return (1);
  * Return: number of integrer
 */
 
-int print_int(va_list args)
+int print_int(va_list args) /* print a int */
 {
-int num = va_arg(args, int);
-int count = 0;
-if (num < 0)
+int num = va_arg(args, int); /* extraire l'argument de va_list */
+int count = 0;/* compte le  nombre de caracteres à imprimer */
+if (num < 0)/* si nombre négatif */
 {
-count += _putchar('-');
-num = -num;
+count += _putchar('-'); /* afficher le signe négatif */
+num = -num;/* rend le nombre positif pour le traitement ultérieur */
 }
-count += print_num(num);
-return (count);
+count += print_num(num);/* affiche le nombre et retourne le nombre de caractères imprimés */ 
+/* affiche le nombre */
+return (count);/* retourne le total des caractères imprimés */
 }
 
 /**
@@ -81,8 +82,9 @@ return (count);
 int print_num(unsigned int num)
 {
 int count = 0;
-if (num / 10 > 0)
-count += print_num(num / 10);
-count += _putchar((num % 10) + '0');
-return (count);
+if (num / 10 > 0)/* si num est supérieur ou égal à 10, on continue la récursion pour traiter les chiffres */
+count += print_num(num / 10);/* appelrécursif avec le nombre réduit */
+count += _putchar((num % 10) + '0');/* affichage du dernier chiffre (num %10) après avoir traité les chiffres avant */
+/* conversion du dernier chiffre en caractère ASCII et affichage */
+return (count);/*retourn le nombre de caractères imprimés */
 }
